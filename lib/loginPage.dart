862ee837e643286818ad62245884558cb322a18b
SmallwolfiOS <1348748408@qq.com>
2020-01-09 22:41:55 +0800

    明天准备研究下flutter的布局

--- lib/loginPage.dart
+++ lib/loginPage.dart
@@ -1,27 +1,30 @@
 import 'package:flutter/material.dart';
 
+import 'package:flutter/material.dart';
+
 class loginPage extends StatefulWidget {
   final String title;
-//  loginPage(this.title);
-  loginPage({Key key, this.title}):super(key :key);
 
+//  loginPage(this.title);
+  loginPage({Key key, this.title}) : super(key: key);
 
   @override
   _loginPageState createState() => _loginPageState();
 }
 
 class _loginPageState extends State<loginPage> {
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
         appBar: AppBar(
-          title: Text(null != widget.title? widget.title:'默认标题'),
+          title: Text(null != widget.title ? widget.title : '默认标题'),
           backgroundColor: Colors.red,
         ),
         body: Center(
-          child: Column(),
+          child: new Scaffold(
+            body: new Text('asd',style: new TextStyle(color: Colors.red,fontSize: 13 ,backgroundColor: Colors.blue ),),
+          ),
         ) // This trailing comma makes auto-formatting nicer for build methods.
-    );
+        );
   }
 }
--- lib/main.dart
+++ lib/main.dart
@@ -10,7 +10,9 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return new MaterialApp(
-      home: loginPage(title: '登录页面',),
+      home: loginPage(
+        title: '登录页面',
+      ),
     );
   }
 }
