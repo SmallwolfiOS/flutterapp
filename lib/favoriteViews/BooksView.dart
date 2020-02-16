f1f4dcd24aaced3b7d4c1ddf583843137f4a3244
SmallwolfiOS <1348748408@qq.com>
2020-02-16 16:53:03 +0800

    添加listView

--- lib/favoriteViews/BooksView.dart
+++ lib/favoriteViews/BooksView.dart
@@ -9,13 +9,25 @@ class BooksView extends StatefulWidget {
 }
 
 class _BooksViewState extends State<BooksView> {
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        body: Center(
-          child: Column(),
-        ) // This trailing comma makes auto-formatting nicer for build methods.
+      body: new ListView.builder(
+          itemCount: 20,
+          itemBuilder: (BuildContext context, int index) {
+            return new Container(
+              height: 40,
+              alignment: Alignment.centerLeft,
+//              color: Colors.blue,
+
+              padding: const EdgeInsets.only(left: 20),
+//          margin: const EdgeInsets.only(top: 10),
+              child: Text('这是第 $index 个cell'),
+              decoration: BoxDecoration(
+                border: Border(bottom: BorderSide(width: 0.5,color: Colors.grey,))
+              ),
+            );
+          }), // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
 }
